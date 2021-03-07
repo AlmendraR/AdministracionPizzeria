59e7a01c6c4b3b17dad7929cf69c7ea1cbaee6d5
Almendra <almendra.rodriguez.s@usach.cl>
2021-03-07 19:48:23 -0300

    asd

--- CapaPresentacion/formulario_login.Designer.cs
+++ CapaPresentacion/formulario_login.Designer.cs
@@ -31,19 +31,20 @@ namespace AdministracionPizzeria.CapaPresentacion
         {
             this.cb_usuariologin = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.bt_aceptarlogin = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.tb_contraseñalogin = new System.Windows.Forms.TextBox();
-            this.bt_aceptarlogin = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // cb_usuariologin
             // 
             this.cb_usuariologin.FormattingEnabled = true;
-            this.cb_usuariologin.Location = new System.Drawing.Point(172, 60);
+            this.cb_usuariologin.Location = new System.Drawing.Point(229, 74);
+            this.cb_usuariologin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cb_usuariologin.Name = "cb_usuariologin";
-            this.cb_usuariologin.Size = new System.Drawing.Size(200, 21);
+            this.cb_usuariologin.Size = new System.Drawing.Size(265, 24);
             this.cb_usuariologin.TabIndex = 0;
             // 
             // panel1
@@ -52,9 +53,24 @@ namespace AdministracionPizzeria.CapaPresentacion
             this.panel1.Controls.Add(this.bt_aceptarlogin);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(150, 176);
+            this.panel1.Size = new System.Drawing.Size(200, 217);
             this.panel1.TabIndex = 1;
+            // 
+            // bt_aceptarlogin
+            // 
+            this.bt_aceptarlogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(167)))), ((int)(((byte)(154)))));
+            this.bt_aceptarlogin.FlatAppearance.BorderSize = 0;
+            this.bt_aceptarlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_aceptarlogin.ForeColor = System.Drawing.Color.Black;
+            this.bt_aceptarlogin.Location = new System.Drawing.Point(32, 49);
+            this.bt_aceptarlogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bt_aceptarlogin.Name = "bt_aceptarlogin";
+            this.bt_aceptarlogin.Size = new System.Drawing.Size(133, 123);
+            this.bt_aceptarlogin.TabIndex = 3;
+            this.bt_aceptarlogin.Text = "Ingresar";
+            this.bt_aceptarlogin.UseVisualStyleBackColor = false;
             // 
             // label2
             // 
@@ -63,9 +79,10 @@ namespace AdministracionPizzeria.CapaPresentacion
             this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(169, 100);
+            this.label2.Location = new System.Drawing.Point(225, 123);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 16);
+            this.label2.Size = new System.Drawing.Size(95, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Contraseña";
             // 
@@ -76,44 +93,34 @@ namespace AdministracionPizzeria.CapaPresentacion
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(169, 40);
+            this.label1.Location = new System.Drawing.Point(225, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 16);
+            this.label1.Size = new System.Drawing.Size(151, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre de usuario";
             // 
             // tb_contraseñalogin
             // 
-            this.tb_contraseñalogin.Location = new System.Drawing.Point(172, 120);
+            this.tb_contraseñalogin.Location = new System.Drawing.Point(229, 148);
+            this.tb_contraseñalogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tb_contraseñalogin.Name = "tb_contraseñalogin";
-            this.tb_contraseñalogin.Size = new System.Drawing.Size(200, 20);
+            this.tb_contraseñalogin.Size = new System.Drawing.Size(265, 22);
             this.tb_contraseñalogin.TabIndex = 2;
-            // 
-            // bt_aceptarlogin
-            // 
-            this.bt_aceptarlogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(167)))), ((int)(((byte)(154)))));
-            this.bt_aceptarlogin.FlatAppearance.BorderSize = 0;
-            this.bt_aceptarlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.bt_aceptarlogin.ForeColor = System.Drawing.Color.Black;
-            this.bt_aceptarlogin.Location = new System.Drawing.Point(24, 40);
-            this.bt_aceptarlogin.Name = "bt_aceptarlogin";
-            this.bt_aceptarlogin.Size = new System.Drawing.Size(100, 100);
-            this.bt_aceptarlogin.TabIndex = 3;
-            this.bt_aceptarlogin.Text = "Ingresar";
-            this.bt_aceptarlogin.UseVisualStyleBackColor = false;
             // 
             // formulario_login
             // 
             this.AcceptButton = this.bt_aceptarlogin;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(164)))), ((int)(((byte)(66)))));
-            this.ClientSize = new System.Drawing.Size(396, 176);
+            this.ClientSize = new System.Drawing.Size(528, 217);
             this.Controls.Add(this.tb_contraseñalogin);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cb_usuariologin);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "formulario_login";
             this.Text = "Form2";
             this.panel1.ResumeLayout(false);
--- CapaPresentacion/formulario_login.cs
+++ CapaPresentacion/formulario_login.cs
@@ -16,5 +16,6 @@ namespace AdministracionPizzeria.CapaPresentacion
         {
             InitializeComponent();
         }
+
     }
 }
