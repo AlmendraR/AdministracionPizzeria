82a35beb777401da967b0f3b216b7b822ad6529a
Marco <marco.gonzalez.s@usach.cl>
2021-03-08 04:32:50 -0300

    sda

--- CapaPresentacion/formulario_login.cs
+++ CapaPresentacion/formulario_login.cs
@@ -2,12 +2,40 @@
 
 namespace AdministracionPizzeria.CapaPresentacion
 {
-    public partial class formulario_login : Form
+    public partial class Formulario_login : Form
     {
-        public formulario_login()
+        public Formulario_login()
         {
             InitializeComponent();
         }
 
+        private void Validarlogin()
+        {
+            if(cb_usuariologin.Text != "Admin")
+            {
+                MessageBox.Show("Usuario incorrecto");
+            }
+            else if (tb_contraseñalogin.Text != "admin")
+            {
+                MessageBox.Show("Contraseña incorrecta");
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void tb_contraseñalogin_Click(object sender, System.EventArgs e)
+        {
+            if (tb_contraseñalogin.Text == "Contraseña")
+            {
+                tb_contraseñalogin.Clear();
+            }
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            Validarlogin();
+        }
     }
 }
